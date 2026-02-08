b957d9ee2e5ec5046083467b7be841b3c7f2e2e9
bagasdprs <bagasdprs@gmail.com>
2026-02-08 23:17:48 +0700

    Feat(Services): Add logic for calculating category assets and stock statistics

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-// import { Repository, ILike, FindOptionsWhere } from 'typeorm';
 import { Repository } from 'typeorm';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
@@ -26,33 +25,37 @@ export class CategoriesService {
     page: number = 1,
     limit: number = 8,
   ): Promise<{ data: any[]; total: number }> {
-    // 1. Inisialisasi QueryBuilder (tetap pertahankan hitungan produk)
     const queryBuilder = this.categoryRepository
       .createQueryBuilder('category')
       .leftJoinAndSelect('category.products', 'product')
       .loadRelationCountAndMap('category.productCount', 'category.products');
 
-    // 2. Jika ada pencarian
     if (search) {
       queryBuilder.where('category.name ILIKE :search', {
         search: `%${search}%`,
       });
     }
 
-    // 3. Tambahkan Logika Pagination
     queryBuilder
       .orderBy('category.id', 'DESC')
-      .take(limit) // Ambil sekian data
-      .skip((page - 1) * limit); // Lewati sekian data
+      .take(limit)
+      .skip((page - 1) * limit);
 
-    // 4. Ambil data dan total baris sekaligus
     const [data, total] = await queryBuilder.getManyAndCount();
 
     return { data, total };
   }
 
   async findOne(id: number) {
-    return await this.categoryRepository.findOne({ where: { id } });
+    const category = await this.categoryRepository.findOne({
+      where: { id },
+      relations: ['products'],
+    });
+
+    if (!category) {
+      throw new Error(`Category with ID ${id} not found`);
+    }
+    return category;
   }
 
   async update(id: number, updateCategoryDto: UpdateCategoryDto) {
